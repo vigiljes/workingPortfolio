8890a9058e89c6c1ee1a713636c366854019076f
vigiljes <vigiljes@msu.edu>
2025-04-07 11:15:05 -0400

    wowBRO

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
   <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-        <img className="mx-4 w-15" src={logo} alt="logo" />
+        <img className="mx-4 w-10" src={logo} alt="logo" />
         </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
