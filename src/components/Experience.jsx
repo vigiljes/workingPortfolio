4571bc01f5e29566d7ac7a0480f126777bb2cea8
vigiljes <vigiljes@msu.edu>
2025-04-23 11:33:38 -0400

    netlifyScaredMeWow

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,7 +8,7 @@ const Experience = () => {
     whileInView={{opacity:1, y: 0}}
     initial={{opacity: 0, y:-102}}
     transition={{duration: 0.5}}
-    className="my-20 text-center text-4xl"><a href={resumeSOhardTOfindWHYYY} target="_blank">🔗Resume</a></motion.h2>
+    className="my-20 text-center text-4xl"><a href={resumeSOhardTOfindWHYYY} target="_blank">🔗 Resume</a></motion.h2>
     <div>
         {EXPERIENCES.map((experience, index) => (
             <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
